ec94f23d691a3754845a64eeab78df7620d23440
mina <2231081064@uttarauniversity.eduu.bd>
2023-05-16 19:18:52 +0600

    ...

--- index.js
+++ index.js
@@ -1,123 +1,115 @@
 
-// const stripe = require('stripe')('sk_test_51M0QGtCx996FZZgar0EDav42cUAomy2QXE4UIeae8WglFKFD7VtyfUx2Jkgkaw9hEMyJ9pPLZ2eqJbngBHZdkozK00YBZqs9VM');
-// const express = require('express');
-// const nodemailer = require('nodemailer');
-// const mongoose = require('mongoose');
-// const cors = require("cors")
-// // const bodyParser=require("body-parser")
-// const c_u = "mongodb+srv://blur:geAUpi2sSPs0Civh@cluster0.c7pvsmn.mongodb.net/?retryWrites=true&w=majority"
-// const app = express();
-// const uuid4 = require("uuid4");
-// const User = require('./data.js');
-// app.use(cors())
-// const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+const stripeRoute = require('./routes/stripeRoute');
+const c_u = "mongodb+srv://blur:geAUpi2sSPs0Civh@cluster0.c7pvsmn.mongodb.net/?retryWrites=true&w=majority"
+const Appsumo=require("./Appsumocode")
+// CREATING APP
+const app = express();
+app.use(
+  cors({
+    origin: '*',
+  })
+);
+mongoose.connect(c_u, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+mongoose.connection.once('open', () => {
+  console.log("Connected Successfully")
 
-// mongoose.connect(c_u, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// })
-// mongoose.connection.once('open', () => {
-//   console.log("Connected Successfully")
+})
 
-// })
-// const transporter = nodemailer.createTransport({
-//   service: 'gmail',
-//   auth: {
-//     user: 'sayem.mia@northsouth.edu',
-//     pass: 'sayambd5522'
-//   }
-// });
+app.use((req, res, next) => {
+  if (req.originalUrl === '/api/v1/stripe/webHook') {
+    console.log(req.originalUrl)
+    next();
+  } else {
+    express.json({ limit: '10mb' })(req, res, next);
+  }
+});
 
+app.post('/appsumo', (req, res) => {
+  const items = req.body;
+  console.log(items) // Assuming the array of items is provided in the request body
 
-// app.use((req, res, next) => {
+  // Insert the array of items into the database
+  Appsumo.insertMany(items)
+    .then(() => {
+      res.status(200).json({ message: 'Items inserted successfully' });
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'Failed to insert items' });
+    });
+});
+app.get("/appsumo",async(req,res)=>{
 
-//   if (req.originalUrl === '/webhook') {
-//     next(); // Do nothing with the body because I need it in a raw state.
-//   } else {
-//     express.json()(req, res, next);  // ONLY do express.json() if the received request is NOT a WebHook from Stripe.
-//   }
-// });
+  try {
+   const data=await Appsumo.find({})
+   if(data){
+    return res.status(200).send(data)
+   }
+  } catch (error) {
+    console.log(error)
+  }
 
-// // This is your Stripe CLI webhook secret for testing your endpoint locally.
-// const endpointSecret = "whsec_NrHgb6PkXxZahXC5293FLnfnjhr37lNL";
-// // express.raw({type: 'application/json'}),
-// app.post('/webhook', async (request, response) => {
+})
 
-//   const sig = request.headers['stripe-signature'];
-//   let event;
+app.get("/allCode", async (req, res) => {
+  try {
+    const item = await User.find({})
+    res.status(201).send(item)
+  } catch (error) {
+    console.log(error)
+  }
+})
+app.put('/appsumo/:code', (req, res) => {
+  const code = req.params.code; // Get the code from the route parameters
 
-//   const stripePayload = request.body;
-//   console.log({stripePayload});
-//   try {
-//     event = stripe.webhooks.constructEvent(stripePayload, sig, endpointSecret);
-//     console.log(("Tye block executeate successfully!"))
-//   } catch (err) {
-//     console.log("catch block executed")
-//     response.status(400).send(`Webhook Error: ${err.message}`);
-//     return;
-//   }
+  // Find the Appsumo document with the matching code
+  Appsumo.findOne({ code })
+    .then((appsumo) => {
+      if (!appsumo) {
+        // Code not found in the database
+        return res.status(404).json({ error: 'Code not found' });
+      }
 
-//   // Handle the event
-//   switch (event.type) {
-//     case 'checkout.session.completed':
-//       const invoiceUpcoming = event.data.object;
-//       console.log(invoiceUpcoming.customer_details.email)
-//       const itemCode = uuid4()
-//       try {
-//         // const item={
-//         //   email:invoiceUpcoming.customer_details.email,
-//         //   code:itemCode
-//         // }
-//         await new User({
-//           email: invoiceUpcoming.customer_details.email,
-//           code: itemCode
-//         }).save()
-
-//       } catch (error) {
-//         console.log(error)
-
-//       }
-
-//       const mailOptions = {
-//         from: 'sayem.mia@northsouth.edu',
-//         to: 'md1040582@gmail.com',
-//         subject: 'Blur Fucas Access Code.',
-//         text: `Your Access Code For Blur Focus is "${itemCode}"`
-//       };
-
-//       transporter.sendMail(mailOptions, function (error, info) {
-//         if (error) {
-//           console.log(error);
-//         } else {
-//           console.log('Email sent: ' + info.response);
-//         }
-//       });
-//       break;
-
-//   }
-//   response.status(200).end()
-// });
-
-// app.get('/webhook',(req,res)=>{
-//    res.send("hello worldd")
-// })
-// app.get('/',(req,res)=>{
-//    res.send("hello worldd")
-// })
+      if (appsumo.turn === 0) {
+        // Turn number is already 0
+        return res.status(200).json({ message: 'Trial number has finished' });
+      }
+      appsumo.turn -= 1;
+      return appsumo.save();
+    })
+    .then(() => {
+      res.status(200).json({ message: 'Trial number updated successfully' });
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'Failed to update turn number' });
+    });
+});
 
 
 
+const PORT = process.env.PORT || 4242;
 
 
+app.use('/api/v1/stripe', stripeRoute);
 
-// app.get("/allCode", async (req, res) => {
-//   try {
-//     const item = await User.find({})
-//     res.status(201).send(item)
-//   } catch (error) {
-//     console.log(error)
-//   }
-// })
-// // app.use(express.json())
-
-// app.listen(4242, () => console.log('Running on port 4242'));
+// ROOT API
+app.get('/', async (req, res) => {
+  try {
+    res.send({
+      runningOn: process.env.NODE_ENV, 
+      isSuccess: true,
+      date: '3/9/2023', //last deploy date
+    });
+  } catch (e) {
+    res.send({ e: e });
+  }
+});
+  app.listen(process.env.PORT || 4242, () => {
+    console.log('server started');
+  });
